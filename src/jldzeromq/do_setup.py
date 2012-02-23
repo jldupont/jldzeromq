431e9349e38fce45566b6236e99595d49eea9818
jldupont <github@jldupont.com>
2012-02-23 16:47:58 -0500

    0.2.7

--- src/jldzeromq/do_setup.py
+++ src/jldzeromq/do_setup.py
@@ -12,7 +12,7 @@ sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 name=os.path.basename(sys.argv[0])
 fname="%-"+str(ALIGN)+"s : %s" % name
 
-FORMAT='%(asctime)s - '+name+' - %(levelname)s - %(message)s'
+FORMAT='%(asctime)s - '+fname+' - %(levelname)s - %(message)s'
 formatter = logging.Formatter(FORMAT)
 
 logging.basicConfig(level=logging.INFO, format=FORMAT)
