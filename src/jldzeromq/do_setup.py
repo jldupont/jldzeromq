b3652acdcd9a34d3b7f7fa0f56ef683dbdbd4bab
jldupont <github@jldupont.com>
2012-02-23 16:52:49 -0500

    0.2.11

--- src/jldzeromq/do_setup.py
+++ src/jldzeromq/do_setup.py
@@ -7,7 +7,7 @@ import logging, sys, os
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
 name=os.path.basename(sys.argv[0])
-fname="%-15s" % name
+fname="%-10s" % name
 
 FORMAT='%(asctime)s - '+fname+' - %(levelname)s - %(message)s'
 formatter = logging.Formatter(FORMAT)
