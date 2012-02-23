b069889c2393cc4834dcb542379de03fa350dcd4
jldupont <github@jldupont.com>
2012-02-23 16:49:05 -0500

    0.2.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
     @author: jldupont
 """
 __author__  ="Jean-Lou Dupont"
-__version__ ="0.2.8"
+__version__ ="0.2.9"
 
 DESC="""
 Overview
--- src/jldzeromq/do_setup.py
+++ src/jldzeromq/do_setup.py
@@ -10,7 +10,7 @@ ALIGN=15
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
 name=os.path.basename(sys.argv[0])
-fname="%-"+str(ALIGN)+"s : %s" % name
+fname="%-"+str(ALIGN)+"s" % name
 
 FORMAT='%(asctime)s - '+fname+' - %(levelname)s - %(message)s'
 formatter = logging.Formatter(FORMAT)
